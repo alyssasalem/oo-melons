5e261df83a6f771ac12e925e0dd55ab259fe7908
alyssasalem <alyssabsalem@gmail.com>
2021-11-11 20:52:52 +0000

    Finished part 3

---
+++ melons.py
@@ -0,0 +1,82 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """Abstract base class for melon orders."""
+    def __init__(self, species, qty, order_type, tax):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        if self.species != 'Christmas melon':
+            total = (1 + self.tax) * self.qty * base_price
+        else:
+            total = (1 + self.tax) * self.qty * (base_price * 1.5)
+
+        if self.order_type == "international" and self.qty < 10:
+            total = total + 3
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+    
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order by the government"""
+    def __init__(self, species, qty):
+        """Initialize government order."""
+        self.species = species
+        self.qty = qty
+        self.order_type = "government"
+        self.tax = 0.0
+        self.passed_inspection = False
+
+    
+    def marked_inspection(self, passed):
+        """Mark whether the order has passed inspection."""
+        self.passed_inspection = passed
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+        
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+order0 = GovernmentMelonOrder("Christmas melon", 6)
+order1 = GovernmentMelonOrder("watermelon", 6)
+
+print(order0.get_total())
+print(order1.get_total())
